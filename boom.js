ce864088615f2b7bb25107ceae153c9cc48215b6
Boost E2E tester script <eng+e2e-tester-script@boostsecurity.io>
2021-04-22 22:15:08 +0000

    Commit eval with expression

---
+++ boom.js
@@ -0,0 +1,6 @@
+
+				// we want the content to have some randomness to be able to push continuously
+				var version1 = 0.9679348719132144
+		
+				eval(getEvilCodeSomewhere() + Math.random())
+			
