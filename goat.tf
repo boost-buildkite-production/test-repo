0ac49956ba6dc5ff3abd2be2eae8f27edadb735f
Boost E2E tester script <eng+e2e-tester-script@boostsecurity.io>
2021-04-27 09:29:43 +0000

    Remove S3 logging

--- goat.tf
+++ goat.tf
@@ -1,15 +1,11 @@
 
-# Goat.tf version 0.33000426901699176
+# Goat.tf version 0.534888069084319
 resource "aws_s3_bucket" "flowbucket" {
 bucket        = "${local.resource_prefix.value}-flowlogs"
 force_destroy = true
 tags = {
 Name        = "${local.resource_prefix.value}-flowlogs"
 Environment = local.resource_prefix.value
-}
-logging {
-target_bucket = "${aws_s3_bucket.logs.id}"
-target_prefix = "log/flowbucket"
 }
 }
 resource "aws_s3_bucket" "flowbucket-2" {
