27f0deb2f235005d4bd644d41580be6cb2c9bbf9
Boost E2E tester script <eng+e2e-tester-script@boostsecurity.io>
2021-05-10 16:53:00 +0000

    Add S3 logging and AWS creds

--- goat.tf
+++ goat.tf
@@ -1,5 +1,5 @@
 
-# Goat.tf version 0.6053863524894774
+# Goat.tf version 0.8375963996588824
 resource "aws_s3_bucket" "flowbucket" {
 bucket        = "${local.resource_prefix.value}-flowlogs"
 force_destroy = true
@@ -14,9 +14,14 @@ target_prefix = "log/flowbucket"
 }
 resource "aws_s3_bucket" "flowbucket-2" {
 bucket        = "${local.resource_prefix.value}-flowlogs-2"
+acl           = "public-read"
 force_destroy = true
 tags = {
 Name        = "${local.resource_prefix.value}-flowlogs-2"
 Environment = local.resource_prefix.value
+}
+logging {
+target_bucket = "${aws_s3_bucket.logs.id}"
+target_prefix = "log/flowbucket"
 }
 }
