0a50b120bdff067222c6fda43b0dbac8ef7030a8
Boost E2E tester script <eng+e2e-tester-script@boostsecurity.io>
2021-05-10 23:27:29 +0000

    Add S3 server side encryption

--- goat.tf
+++ goat.tf
@@ -1,5 +1,5 @@
 
-# Goat.tf version 0.0028948283293062094
+# Goat.tf version 0.6384370978567813
 resource "aws_s3_bucket" "flowbucket" {
 bucket        = "${local.resource_prefix.value}-flowlogs"
 force_destroy = true
@@ -10,6 +10,14 @@ Environment = local.resource_prefix.value
 logging {
 target_bucket = "${aws_s3_bucket.logs.id}"
 target_prefix = "log/flowbucket"
+}
+server_side_encryption_configuration {
+rule {
+apply_server_side_encryption_by_default {
+kms_master_key_id = aws_kms_key.mykey.arn
+sse_algorithm     = "aws:kms"
+}
+}
 }
 }
 resource "aws_s3_bucket" "flowbucket-2" {
@@ -23,5 +31,13 @@ Environment = local.resource_prefix.value
 logging {
 target_bucket = "${aws_s3_bucket.logs.id}"
 target_prefix = "log/flowbucket"
+}
+server_side_encryption_configuration {
+rule {
+apply_server_side_encryption_by_default {
+kms_master_key_id = aws_kms_key.mykey.arn
+sse_algorithm     = "aws:kms"
+}
+}
 }
 }
