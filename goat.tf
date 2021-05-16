ba8e1cd9c148218e6bf0e0ec5956bff114d3d1f4
Boost E2E tester script <eng+e2e-tester-script@boostsecurity.io>
2021-05-16 08:23:03 +0000

    Commit a vulerability

--- goat.tf
+++ goat.tf
@@ -1,2 +1,14 @@
 
-# Goat.tf version 0.21609798778778
+# Goat.tf version 0.5059141976664328
+resource "aws_s3_bucket" "flowbucket" {
+bucket        = "${local.resource_prefix.value}-flowlogs"
+force_destroy = true
+logging {
+target_bucket = "${aws_s3_bucket.logs.id}"
+target_prefix = "log/"
+}
+tags = {
+Name        = "${local.resource_prefix.value}-flowlogs"
+Environment = local.resource_prefix.value
+}
+}
