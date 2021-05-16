8f6eac9037ab6727abb1d70cd0f1c02712709cd9
Boost E2E tester script <eng+e2e-tester-script@boostsecurity.io>
2021-05-16 08:23:24 +0000

    Fix unencrypted S3 bucket

--- goat.tf
+++ goat.tf
@@ -1,14 +1,22 @@
 
-# Goat.tf version 0.5059141976664328
+# Goat.tf version 0.6995662477697244
 resource "aws_s3_bucket" "flowbucket" {
 bucket        = "${local.resource_prefix.value}-flowlogs"
 force_destroy = true
+tags = {
+Name        = "${local.resource_prefix.value}-flowlogs"
+Environment = local.resource_prefix.value
+}
 logging {
 target_bucket = "${aws_s3_bucket.logs.id}"
 target_prefix = "log/"
 }
-tags = {
-Name        = "${local.resource_prefix.value}-flowlogs"
-Environment = local.resource_prefix.value
+server_side_encryption_configuration {
+rule {
+apply_server_side_encryption_by_default {
+sse_algorithm     = "aws:kms"
+kms_master_key_id = "${aws_kms_key.logs_key.arn}"
+}
+}
 }
 }
