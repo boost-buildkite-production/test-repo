8790ebbcbf9689da92ec515c9ed1057cfbe3d143
Boost E2E tester script <eng+e2e-tester-script@boostsecurity.io>
2021-05-17 23:40:02 +0000

    Add S3 logging

--- goat.tf
+++ goat.tf
@@ -1,10 +1,14 @@
 
-# Goat.tf version 0.8509795461661791
+# Goat.tf version 0.5852457026008895
 resource "aws_s3_bucket" "flowbucket" {
 bucket        = "${local.resource_prefix.value}-flowlogs"
 force_destroy = true
 tags = {
 Name        = "${local.resource_prefix.value}-flowlogs"
 Environment = local.resource_prefix.value
+}
+logging {
+target_bucket = "${aws_s3_bucket.logs.id}"
+target_prefix = "log/"
 }
 }
